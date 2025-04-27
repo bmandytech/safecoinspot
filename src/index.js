748d2937cea729a3b40e3ff6f1e2a702e600159c
Medina Sherry <medinasherry6@gmail.com>
2025-04-27 11:14:22 -0700

    src

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom'; // Changed to match React 17 import
 import App from './App'; // Ensure App.js is properly named with capital 'A'
 import './styles/tailwind.css'; // Import Tailwind CSS
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production ') disableReactDevTools()
 
 // Create a root element and render the App component
 const root = document.getElementById('root');
